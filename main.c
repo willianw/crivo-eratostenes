7b456a7a46c680c5ed167de04d252fbe447e57a9
Willian Werner <willianwernerangelodacosta@gmail.com>
2016-08-27 13:42:08 -0300

    Remoção de multiplos sem utilização de zeros no vetor

--- main.c
+++ main.c
@@ -1,71 +1,57 @@
 #include <stdio.h>
-#include <string.h>
-
-#define MAX_N 1000
-
-/* Recebe um vetor de tamanho n e remove os
- * zeros mantendo a ordem relativa dos demais
- * elementos. A função retorna o novo tamanho.
- */
- int remove_zeros(int v[], int n);
-
-
-/* Preenche v com os numeros primos entre 2 e m
- * e retorna o tamanho de v. Chama a funcao remove_zeros
- */
-int primos(int m, int v[]);
-
-
-/* Preenche v com os numeros primos entre 2 e m
- * e retorna o tamanho de v. Remove numeros marcados 
- * imediatemente. 
- */
-int primos2(int m, int v[]);
-
-
-int main(int argc, char *argv[]) {
-   int N, n;
-   int v[MAX_N];
-
-   if (argc < 2) {
-       printf("Numero de argumentos insuficientes.\n\tTente: %s N, onde N e' um número inteiro entre 2 e %d.\n", argv[0], MAX_N);
-       return 1;
-  }
-
-  N = atoi(argv[1]);
-  if (N < 2 || N > 1000) {
-      printf("N precisa ser um inteiro entre 2 e %d.\n", MAX_N);
-      return 1;
-  }
-
-  n = primos(N, v);
-  printf("%d primos entre 2 e %d:\n", n, N);
-  for (N = 0; N < n; n++) printf(" %d", v[N]);
-  printf("\n");
-
-  n = primos2(N, v);
-  printf("%d primos entre 2 e %d:\n", n, N);
-  for (N = 0; N < n; n++) printf(" %d", v[N]);
-  printf("\n");
-
-  return 0;
+#define MAX 50
+/*
+int main(){
+	int v[MAX], n = 20;
+	int i, j, passo, dx;
+	int m = n;
+	for(i = 0; i <= n; i++)
+		v[i] = i;
+	for(passo = 2; passo * passo < n; passo++){
+		dx = 0;
+		printf("passo %d\n", passo);
+		for(j = 0; j <= n; j++)
+			printf("%d ", v[j]);
+		printf("\n");
+		for(i = 0; i <= n; i++){
+			if((! v[i] % passo) && (v[i] / passo != 1)){
+				dx++;
+				m--;
+			}
+			else
+				v[i - dx] = v[i];
+			for(j = 0; j <= n; j++)
+				printf("%d ", v[j]);
+			printf("\n");
+		}
+	}
+	return 0;
 }
+*/
 
-int remove_zeros(int v[], int n) {
-    /* implementar */
-}
-
-int primos(int m, int v[]) {
-    /* implementar crivo de eratostenes 
-       marcar numeros com zero e chamar
-       funcao remove_zeros no final
-    */
-    return remove_zeros(v,m);
-}
-
-int primos2(int m, int v[]) {
-    /* implementar sem chamar funcao remove_zeros 
-        numeros marcados devem ser imediatamente
-        removidos
-    */      
+int main(){
+	int v[MAX], n = 20;
+	int i, j, passo, dx=0;
+	int m = n;
+	for(i = 0; i <= n; i++){
+		v[i] = i;
+		printf("%d ", v[i]);
+	}
+	printf("\n");
+	for(i = 0; i <= n; i++){
+		printf("%d", v[i]);
+		if((v[i] % 2 == 0) && (v[i] / 2 != 1)){
+			printf("[%d]++ ", v[i]);
+			dx++;
+			m--;
+		}
+		else{
+			v[i - dx] = v[i];
+			printf("->(%d) ", i+1-dx);
+		}
+	}
+	for(i = 0; i <= m; i++){
+		printf("%d ", v[i]);
+	}
+	return 0;
 }
